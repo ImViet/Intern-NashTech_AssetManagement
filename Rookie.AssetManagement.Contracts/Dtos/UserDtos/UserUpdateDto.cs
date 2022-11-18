67f921c6fb34453212c98c3ea817a703cfd01bea
ImViet <dinhquocviet318@gmail.com>
2022-11-18 22:57:57 +0700

    Create UserDtos

---
+++ Rookie.AssetManagement.Contracts/Dtos/UserDtos/UserUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using Rookie.AssetManagement.Contracts.Dtos.EnumDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rookie.AssetManagement.Contracts.Dtos.UserDtos
+{
+    public class UserUpdateDto
+    {
+        public DateTime DateOfBirth { get; set; }
+        public UserGenderEnumDto Gender { get; set; }
+        public DateTime JoinedDate { get; set; }
+        public string Type { get; set; }
+    }
+}
