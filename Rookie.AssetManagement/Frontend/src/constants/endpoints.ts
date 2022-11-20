4b56580c55fed6fea26f4041044a825b91c6f519
Ngo Vo Tuan An <ngovotuananh@gmail.com>
2022-11-20 21:30:39 +0700

    Resolve Conflict

--- Rookie.AssetManagement/Frontend/src/constants/endpoints.ts
+++ Rookie.AssetManagement/Frontend/src/constants/endpoints.ts
@@ -1,9 +1,6 @@
 const Endpoints = {
   authorize: "api/authorize",
   me: "api/authorize/me",
-
-  user: "api/users",
-  userId: (id: number | string): string => `api/users/${id}`,
 };
 
 export default Endpoints;
