a43c902eb14b7f698bde1c036fe38c6d9713d554
ImViet <dinhquocviet318@gmail.com>
2022-11-20 19:44:23 +0700

    Update UserDto

--- Rookie.AssetManagement.Contracts/Dtos/UserDtos/UserDto.cs
+++ Rookie.AssetManagement.Contracts/Dtos/UserDtos/UserDto.cs
@@ -12,7 +12,11 @@ namespace Rookie.AssetManagement.Contracts.Dtos.UserDtos
         public int Id { get; set; }
         public string StaffCode { get; set; }
         public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string UserName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Gender {get; set;}
         public DateTime JoinedDate { get; set; }
         public string Type { get; set; }
         
