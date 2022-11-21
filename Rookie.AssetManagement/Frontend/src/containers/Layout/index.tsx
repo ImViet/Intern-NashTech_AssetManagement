5ddf708714dca2f6aa5cb193a4fe661f8d232f0c
DatNgo <ngominhdat110115@gmail.com>
2022-11-21 16:36:26 +0700

    fix login form and header

--- Rookie.AssetManagement/Frontend/src/containers/Layout/index.tsx
+++ Rookie.AssetManagement/Frontend/src/containers/Layout/index.tsx
@@ -5,7 +5,7 @@ import Header from "./Header";
 import SideBar from "./SideBar";
 import { Route, Outlet } from "react-router-dom";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, showSideBar = true }) => {
   return (
     <>
       <NotificationContainer />
@@ -14,11 +14,13 @@ const Layout = ({ children }) => {
       <div className="container-lg-min container-fluid">
         <div className="row mt-5">
 
-          <div className="col-lg-2 col-md-4 col-12 mr-5">
-            <SideBar />
-          </div>
+          {showSideBar && (
+            <div className="col-lg-2 col-md-4 col-12 mr-5">
+              <SideBar />
+            </div>
+          )}
 
-          <div className="col-lg-9 col-md-7 ms-5">
+          <div className={`${showSideBar?"col-lg-9 col-md-7":"col-12"} ms-5`}>
             {children}
           </div>
         </div>
