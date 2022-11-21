cbdc0425c3fd8b66916f5c3ff0dc1cebcd18c2f1
Hoàng Hữu Đông <89037661+HoangHuuDong@users.noreply.github.com>
2022-11-21 16:38:14 +0700

    add create form

--- Rookie.AssetManagement/Frontend/src/constants/selectOptions.ts
+++ Rookie.AssetManagement/Frontend/src/constants/selectOptions.ts
@@ -5,3 +5,8 @@ export const UserTypeOptions: ISelectOption[] = [
     { id: 2, label: 'Admin', value: 'ADMIN' },
     { id: 3, label: 'Staff', value: 'STAFF' },
 ];
+
+export const GenderOptions: ISelectOption[] = [
+    { id: 1, label: "Female", value: "Female"},
+    { id: 2, label: "Male", value: "Male"},
+];
