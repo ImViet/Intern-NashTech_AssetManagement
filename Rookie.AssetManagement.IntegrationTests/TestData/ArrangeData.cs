1b2bc4a51d8b61814f7bf41c0d2231773f8fb09e
Duong Trieu <duongtrieu54@gmail.com>
2022-11-21 13:06:43 +0700

    Integration Test

--- Rookie.AssetManagement.IntegrationTests/TestData/ArrangeData.cs
+++ Rookie.AssetManagement.IntegrationTests/TestData/ArrangeData.cs
@@ -1,11 +1,80 @@
 using System;
+<<<<<<< HEAD
 using Rookie.AssetManagement.Constants;
+=======
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Rookie.AssetManagement.DataAccessor.Data;
+using Rookie.AssetManagement.DataAccessor.Enum;
+>>>>>>> 621820f (Integration Test)
 using Rookie.AssetManagement.DataAccessor.Entities;
+using Rookie.AssetManagement.Contracts.Dtos.UserDtos;
+using Rookie.AssetManagement.Contracts.Dtos.EnumDtos;
 
 namespace Rookie.AssetManagement.IntegrationTests.TestData
 {
     public static class ArrangeData
     {
+<<<<<<< HEAD
   
+=======
+        public static List<User> GetSeedUsersData()
+        {
+            return new List<User>()
+            {
+                new User()
+                {
+                    FirstName = "First Name 1",
+                    LastName = "Last Name 1",
+                    DateOfBirth = new DateTime(2000, 11, 31, 0, 0, 0),
+                    Gender = UserGenderEnum.Male,
+                    JoinedDate = new DateTime(2022, 11, 10, 0, 0, 0),
+                    Type = "Staff",
+                },
+                new User()
+                {
+                    FirstName = "First Name 2",
+                    LastName = "Last Name 2",
+                    DateOfBirth = new DateTime(2000, 05, 15, 0, 0, 0),
+                    Gender = UserGenderEnum.Male,
+                    JoinedDate = new DateTime(2022, 11, 10, 0, 0, 0),
+                    Type = "Staff",
+                },
+                new User()
+                {
+                    FirstName = "First Name 3",
+                    LastName = "Last Name 3",
+                    DateOfBirth = new DateTime(2000, 02, 17, 0, 0, 0),
+                    Gender = UserGenderEnum.Female,
+                    JoinedDate = new DateTime(2022, 11, 10, 0, 0, 0),
+                    Type = "Staff",
+                },
+                
+                
+            };
+        }
+
+        public static UserCreateDto GetCreateUserDto()
+        {
+            return new UserCreateDto() {
+
+                FirstName = "First Name 4",
+                LastName = "Last Name 4",
+                DateOfBirth = new DateTime(2000, 02, 17, 0, 0, 0),
+                Gender = UserGenderEnumDto.Female,
+                JoinedDate = new DateTime(2022, 11, 10, 0, 0, 0),
+                Type = "Staff",
+
+            };
+        }
+
+        public static void InitUsersData(ApplicationDbContext dbContext)
+        {
+            var users = GetSeedUsersData();
+            dbContext.Users.AddRange(users);
+            dbContext.SaveChanges();
+        }
+
+>>>>>>> 621820f (Integration Test)
     }
 }
