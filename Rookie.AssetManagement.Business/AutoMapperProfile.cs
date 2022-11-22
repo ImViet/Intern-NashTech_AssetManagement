d2535e23fc903ccf227b05c9e3064ae1f3b758f3
DatNgo <ngominhdat110115@gmail.com>
2022-11-22 14:28:13 +0700

    fix auto mapper

--- Rookie.AssetManagement.Business/AutoMapperProfile.cs
+++ Rookie.AssetManagement.Business/AutoMapperProfile.cs
@@ -31,8 +31,29 @@ namespace Rookie.AssetManagement.Business
                 .ForMember(d => d.TwoFactorEnabled, t => t.Ignore())
                 .ForMember(d => d.LockoutEnd, t => t.Ignore())
                 .ForMember(d => d.LockoutEnabled, t => t.Ignore())
-                .ForMember(d => d.AccessFailedCount, t => t.Ignore());
-            CreateMap<UserUpdateDto, User>();
+                .ForMember(d => d.AccessFailedCount, t => t.Ignore())
+                .ForMember(d => d.SecurityStamp, t => t.Ignore())
+                .ForMember(d => d.ConcurrencyStamp, t => t.Ignore());
+            CreateMap<UserUpdateDto, User>()
+                .ForMember(d => d.IsDeleted, t => t.Ignore())
+                .ForMember(d => d.StaffCode, t => t.Ignore())
+                .ForMember(d => d.Location, t => t.Ignore())
+                .ForMember(d => d.IsNewUser, t => t.Ignore())
+                .ForMember(d => d.Id, t => t.Ignore())
+                .ForMember(d => d.UserName, t => t.Ignore())
+                .ForMember(d => d.NormalizedUserName, t => t.Ignore())
+                .ForMember(d => d.Email, t => t.Ignore())
+                .ForMember(d => d.NormalizedEmail, t => t.Ignore())
+                .ForMember(d => d.EmailConfirmed, t => t.Ignore())
+                .ForMember(d => d.PasswordHash, t => t.Ignore())
+                .ForMember(d => d.PhoneNumber, t => t.Ignore())
+                .ForMember(d => d.PhoneNumberConfirmed, t => t.Ignore())
+                .ForMember(d => d.TwoFactorEnabled, t => t.Ignore())
+                .ForMember(d => d.LockoutEnd, t => t.Ignore())
+                .ForMember(d => d.LockoutEnabled, t => t.Ignore())
+                .ForMember(d => d.AccessFailedCount, t => t.Ignore())
+                .ForMember(d => d.SecurityStamp, t => t.Ignore())
+                .ForMember(d => d.ConcurrencyStamp, t => t.Ignore());
         }
 
         private void FromDataAccessorLayer()
@@ -40,7 +61,8 @@ namespace Rookie.AssetManagement.Business
             CreateMap<User, UserDto>()
                 .ForMember(d => d.FullName, t => t.MapFrom(src => src.FirstName + " " + src.LastName));
             CreateMap<User, AccountDto>()
-                .ForMember(d => d.FullName, t => t.MapFrom(src => src.FirstName + " " + src.LastName));
+                .ForMember(d => d.FullName, t => t.MapFrom(src => src.FirstName + " " + src.LastName))
+                .ForMember(d => d.Token, t => t.Ignore());
         }
     }
 }
