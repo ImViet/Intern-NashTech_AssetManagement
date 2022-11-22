cec3a2dc55ee3db21752bffc6edb47fd7ba71356
DatNgo <ngominhdat110115@gmail.com>
2022-11-22 14:22:49 +0700

    fix auto mapper profile

--- Rookie.AssetManagement.Business/AutoMapperProfile.cs
+++ Rookie.AssetManagement.Business/AutoMapperProfile.cs
@@ -16,7 +16,22 @@ namespace Rookie.AssetManagement.Business
         {
             CreateMap<UserCreateDto, User>()
                 .ForMember(d => d.IsDeleted, t => t.Ignore())
-                .ForMember(d => d.StaffCode, t => t.Ignore());
+                .ForMember(d => d.StaffCode, t => t.Ignore())
+                .ForMember(d => d.Location, t => t.Ignore())
+                .ForMember(d => d.IsNewUser, t => t.Ignore())
+                .ForMember(d => d.Id, t => t.Ignore())
+                .ForMember(d => d.UserName, t => t.Ignore())
+                .ForMember(d => d.NormalizedUserName, t => t.Ignore())
+                .ForMember(d => d.Email, t => t.Ignore())
+                .ForMember(d => d.NormalizedEmail, t => t.Ignore())
+                .ForMember(d => d.EmailConfirmed, t => t.Ignore())
+                .ForMember(d => d.PasswordHash, t => t.Ignore())
+                .ForMember(d => d.PhoneNumber, t => t.Ignore())
+                .ForMember(d => d.PhoneNumberConfirmed, t => t.Ignore())
+                .ForMember(d => d.TwoFactorEnabled, t => t.Ignore())
+                .ForMember(d => d.LockoutEnd, t => t.Ignore())
+                .ForMember(d => d.LockoutEnabled, t => t.Ignore())
+                .ForMember(d => d.AccessFailedCount, t => t.Ignore());
             CreateMap<UserUpdateDto, User>();
         }
 
