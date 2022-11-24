be63a72a1699b880dc69830dba9557dc29cd01da
DatNgo <ngominhdat110115@gmail.com>
2022-11-24 09:55:53 +0700

    fix type name

--- Rookie.AssetManagement/Frontend/src/constants/User/UserContants.ts
+++ Rookie.AssetManagement/Frontend/src/constants/User/UserContants.ts
@@ -1,6 +1,6 @@
-export const AdminUserType = "admin";
-export const StaffUserType = "staff";
-export const AllUserType = "all";
+export const AdminUserType = "ADMIN";
+export const StaffUserType = "STAFF";
+export const AllUserType = "ALL";
 
 export const AdminUserTypeLabel = "Admin";
 export const StaffUserTypeLabel = "Staff";
