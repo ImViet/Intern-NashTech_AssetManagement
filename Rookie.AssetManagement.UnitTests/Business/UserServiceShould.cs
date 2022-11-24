f15ba5772a954f33fcea1a0cfadc3029dc18513f
Hoàng Hữu Đông <89037661+HoangHuuDong@users.noreply.github.com>
2022-11-24 16:02:28 +0700

    change new uesername and unitest create

--- Rookie.AssetManagement.UnitTests/Business/UserServiceShould.cs
+++ Rookie.AssetManagement.UnitTests/Business/UserServiceShould.cs
@@ -41,13 +41,13 @@ namespace Rookie.AssetManagement.UnitTests.Business
 
         private readonly CancellationToken _cancellationToken;
 
-         public UserServiceShould()
+        public UserServiceShould()
         {
             _userRepository = new Mock<IBaseRepository<User>>();
             _userManager = new Mock<UserManager<User>>();
             var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperProfile>());
             _mapper = config.CreateMapper();
-            _userService = new UserService(_userRepository.Object, null,_mapper);
+            _userService = new UserService(_userRepository.Object, null, _mapper);
             _cancellationToken = new CancellationToken();
         }
         [Fact]
@@ -73,7 +73,6 @@ namespace Rookie.AssetManagement.UnitTests.Business
         public async Task AddAsyncShouldBeSuccessfullyAsync()
         {
             //Arrange
-
             var ListUser = UserTestData.ListUser().ToList().BuildMock();
             var newUser = _mapper.Map<User>(UserTestData.GetCreateUserDto());
             _userRepository.Setup(x => x.Entities).Returns(ListUser);
@@ -84,6 +83,7 @@ namespace Rookie.AssetManagement.UnitTests.Business
             var result = await _userService.AddAsync(UserTestData.GetCreateUserDto(), "HCM");
             //Assert
             Assert.Equal("Trieu", result.FirstName);
+
         }
 
         [Fact]
