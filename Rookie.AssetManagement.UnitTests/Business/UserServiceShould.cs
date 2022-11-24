fb46d1d366d77a8bd9b034390c4fbf9af163b605
Hoàng Hữu Đông <89037661+HoangHuuDong@users.noreply.github.com>
2022-11-24 18:04:10 +0700

    change

--- Rookie.AssetManagement.UnitTests/Business/UserServiceShould.cs
+++ Rookie.AssetManagement.UnitTests/Business/UserServiceShould.cs
@@ -15,23 +15,17 @@ using Xunit;
 using Rookie.AssetManagement.Business;
 using MockQueryable.Moq;
 using Rookie.AssetManagement.Contracts;
-<<<<<<< HEAD
 using System.Threading;
 using Rookie.AssetManagement.Contracts.Dtos.UserDtos;
 using NPOI.SS.Formula.Functions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Rewrite;
-<<<<<<< HEAD
 using Rookie.AssetManagement.DataAccessor.Migrations;
 using FluentAssertions.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
-=======
->>>>>>> 9d21c86 (change new uesername and unitest create)
-=======
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Rewrite;
->>>>>>> c4ce9e9 (change new uesername and unitest create)
+
+
 
 
 
@@ -95,65 +89,6 @@ namespace Rookie.AssetManagement.UnitTests.Business
         }
 
         [Fact]
-        public async Task AddAsyncShouldThrowExceptionAsync()
-        {
-            Func<Task> act = async () => await _userService.AddAsync(null, null);
-            await act.Should().ThrowAsync<ArgumentNullException>();
-        }
-
-        [Fact]
-        public async Task AddAsyncShouldBeSuccessfullyAsync()
-        {
-            //Arrange
-<<<<<<< HEAD
-            var ListUser = UserTestData.ListUser().ToList().BuildMock();
-            var newUser = _mapper.Map<User>(UserTestData.GetCreateUserDto());
-            _userRepository.Setup(x => x.Entities).Returns(ListUser);
-            _userRepository.Setup(x => x.GetById(It.IsAny<int>())).ReturnsAsync(newUser);
-            _userManager.Setup(x => x.Users).Returns(ListUser);
-            _userManager.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Verifiable();
-            //Act
-            var result = await _userService.AddAsync(UserTestData.GetCreateUserDto(), "HCM");
-            //Assert
-            Assert.Equal("Trieu", result.FirstName);
-=======
-            var newUserId = 1;
-            var user = UserTestData.GetCreateUserDto();
-            var userCreate = await _userService.AddAsync(UserTestData.GetCreateUserDto(), "hcm");
-            //Act
-
-            var result = _userRepository
-               .Setup(x => x.GetById(newUserId))
-              .Returns(Task.FromResult<User>(UserTestData.GetUser(newUserId)));
-            //Assert
-            //Assert.Equal(, );
->>>>>>> 9d21c86 (change new uesername and unitest create)
-        }
-
-        [Fact]
-        public async Task AddAsyncShouldThrowExceptionAsync()
-        {
-            Func<Task> act = async () => await _userService.AddAsync(null, null);
-            await act.Should().ThrowAsync<ArgumentNullException>();
-        }
-
-        [Fact]
-        public async Task AddAsyncShouldBeSuccessfullyAsync()
-        {
-            //Arrange
-            var newUserId = 1;
-            var user = UserTestData.GetCreateUserDto();
-            var userCreate = await _userService.AddAsync(UserTestData.GetCreateUserDto(), "hcm");
-            //Act
-
-            var result = _userRepository
-               .Setup(x => x.GetById(newUserId))
-              .Returns(Task.FromResult<User>(UserTestData.GetUser(newUserId)));
-            //Assert
-            //Assert.Equal(, );
-        }
-
-        [Fact]
         public async Task UpdateAsyncShouldThrowNotFoundException()
         {
             //Arrange
@@ -166,6 +101,7 @@ namespace Rookie.AssetManagement.UnitTests.Business
             //Assert
             await act.Should().ThrowAsync<NotFoundException>();
         }
+        
         [Fact]
         public async Task UpdateAsyncShouldSuccess()
         {
