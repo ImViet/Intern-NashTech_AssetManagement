ac4f4e388fd3afc73eecbacb86766ca3579b281e
Ngo Vo Tuan An <ngovotuananh@gmail.com>
2022-11-28 09:31:10 +0700

    UnitTest Cretae

--- Rookie.AssetManagement.UnitTests/Business/UserServiceShould.cs
+++ Rookie.AssetManagement.UnitTests/Business/UserServiceShould.cs
@@ -48,7 +48,7 @@ namespace Rookie.AssetManagement.UnitTests.Business
         public UserServiceShould()
         {
             _userRepository = new Mock<IBaseRepository<User>>();
-            _userManager = new Mock<UserManager<User>>();
+            _userManager = new Mock<UserManager<User>>(Mock.Of<IUserStore<User>>(), null, null, null, null, null, null, null, null);
             var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperProfile>());
             _mapper = config.CreateMapper();
             _userService = new UserService(_userRepository.Object, _userManager.Object, _mapper);
