33102ec8038c4c805fedbd3450837de27efaf7ed
Ngo Vo Tuan An <ngovotuananh@gmail.com>
2022-11-28 14:32:24 +0700

    BE paging and filter asset

--- Rookie.AssetManagement.Business/AutoMapperProfile.cs
+++ Rookie.AssetManagement.Business/AutoMapperProfile.cs
@@ -2,6 +2,7 @@
 using Rookie.AssetManagement.Contracts.Dtos.UserDtos;
 using Rookie.AssetManagement.Contracts.Dtos.AssetDtos;
 using Rookie.AssetManagement.DataAccessor.Entities;
+using Rookie.AssetManagement.DataAccessor.Data;
 
 namespace Rookie.AssetManagement.Business
 {
