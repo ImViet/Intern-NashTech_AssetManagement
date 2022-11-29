5dca9f1587133fa168fc2ea7fdf13f04df2eb620
DatNgo <ngominhdat110115@gmail.com>
2022-11-29 16:32:37 +0700

    Unit test create and search

---
+++ Rookie.AssetManagement.UnitTests/TestDataAPI/AssetTestData.cs
@@ -0,0 +1,81 @@
+﻿using Rookie.AssetManagement.Contracts.Dtos.AssetDtos;
+using Rookie.AssetManagement.Contracts.Dtos.EnumDtos;
+using Rookie.AssetManagement.Contracts.Dtos.UserDtos;
+using Rookie.AssetManagement.DataAccessor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rookie.AssetManagement.UnitTests.TestDataAPI
+{
+    public class AssetTestData
+    {
+        public static AssetQueryCriteriaDto AssetQueryCriteriaDto = new AssetQueryCriteriaDto()
+        {
+            Search = "Monitor",
+            Page = 1,
+            Limit = 5,
+        };
+
+        public static List<Asset> GetAssets()
+        {
+            return new List<Asset>() {
+                new Asset() {
+                    Id = 1,
+                    AssetCode = "MO000001",
+                    AssetName = "Monitor xyz",
+                    Specification = "abc",
+                    IsDeleted = false,
+                    Location="HCM"
+                },
+                new Asset() {
+                    Id = 2,
+                    AssetCode = "MO000002",
+                    AssetName = "Monitor xyz2",
+                    Specification = "abc",
+                    IsDeleted = false,
+                    Location="HCM"
+                }
+            };
+        }
+        public static List<State> GetStates()
+        {
+            return new List<State>() {
+                new State() {
+                    Id = 1,
+                    StateName="Available",
+                },
+                new State() {
+                    Id = 2,
+                    StateName="Not Available",
+                }
+            };
+        }
+        public static List<Category> GetCategories()
+        {
+            return new List<Category>() {
+                new Category() {
+                    Id = 1,
+                    CategoryName="Monitor",
+                },
+                new Category() {
+                    Id = 2,
+                    CategoryName="Laptop",
+                }
+            };
+        }
+        public static AssetCreateDto GetCreateAssetDto()
+        {
+            return new AssetCreateDto()
+            {
+                AssetName = "Monitor xyz3",
+                Specification = "abc",
+                Category = 1,
+                State = 1
+            };
+        }
+
+    }
+}
