6ffa0463a1af3f8094637759d71f2232b176e899
DatNgo <ngominhdat110115@gmail.com>
2022-11-30 10:01:17 +0700

    get asset form data

--- Rookie.AssetManagement/Frontend/src/containers/Asset/sagas/index.ts
+++ Rookie.AssetManagement/Frontend/src/containers/Asset/sagas/index.ts
@@ -1,10 +1,23 @@
 import { takeLatest } from "redux-saga/effects";
-import { createAsset, getAssetList, getCategory, getState } from "../reducer";
-import { handleCreateAsset, handleGetAssetList, handleGetCategoryList, handleGetStateList } from "./handles";
+import {
+  createAsset,
+  getAssetFormData,
+  getAssetList,
+  getCategory,
+  getState,
+} from "../reducer";
+import {
+  handleCreateAsset,
+  handleGetAssetForm,
+  handleGetAssetList,
+  handleGetCategoryList,
+  handleGetStateList,
+} from "./handles";
 
 export default function* AssetSagas() {
-    yield takeLatest(getAssetList.type, handleGetAssetList);
-    yield takeLatest(getCategory.type, handleGetCategoryList);
-    yield takeLatest(getState.type, handleGetStateList);
-    yield takeLatest(createAsset.type,handleCreateAsset);
+  yield takeLatest(getAssetList.type, handleGetAssetList);
+  yield takeLatest(getCategory.type, handleGetCategoryList);
+  yield takeLatest(getState.type, handleGetStateList);
+  yield takeLatest(createAsset.type, handleCreateAsset);
+  yield takeLatest(getAssetFormData.type, handleGetAssetForm);
 }
