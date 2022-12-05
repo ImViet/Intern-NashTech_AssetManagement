d62f97fe3cee071c435885d77dd0358a8ee674c5
Duong Trieu <duongtrieu54@gmail.com>
2022-12-05 17:24:10 +0700

    fix detail modal

--- Rookie.AssetManagement/Frontend/src/containers/Asset/Info/index.tsx
+++ Rookie.AssetManagement/Frontend/src/containers/Asset/Info/index.tsx
@@ -31,6 +31,7 @@ const Info: React.FC<Props> = ({ asset, handleClose }) => {
                 show={true}
                 onHide={handleClose}
                 dialogClassName="detail-modal model-info-asset modal-90w"
+                centered
             >
                 <Modal.Header closeButton>
                     <Modal.Title id="login-modal">
@@ -41,36 +42,36 @@ const Info: React.FC<Props> = ({ asset, handleClose }) => {
                 <Modal.Body>
                     <div className="table-detail">
                         <div className='row -intro-y'>
-                            <div className='col-3'>Asset Code:</div>
+                            <div className='col-3'>Asset Code</div>
                             <div className='col-4'>{asset.assetCode}</div>
                         </div>
 
                         <div className='row -intro-y'>
-                            <div className='col-3'>Asset Name:</div>
+                            <div className='col-3'>Asset Name</div>
                             <div className='col-4'>{asset.assetName}</div>
                         </div>
                         <div className='row -intro-y'>
-                            <div className='col-3'>Category:</div>
+                            <div className='col-3'>Category</div>
                             <div className='col-4'> {asset.category}</div>
                         </div>
                         <div className='row -intro-y'>
-                            <div className='col-3'>Installed Date:</div>
+                            <div className='col-3'>Installed Date</div>
                             <div className='col-4'>{convertDDMMYYYY(asset.installedDate)}</div>
                         </div>
                         <div className='row -intro-y'>
-                            <div className='col-3'>State:</div>
+                            <div className='col-3'>State</div>
                             <div className='col-4'>{asset.state}</div>
                         </div>
                         <div className='row -intro-y'>
-                            <div className='col-3'>Location:</div>
+                            <div className='col-3'>Location</div>
                             <div className='col-4'>{asset.location}</div>
                         </div>
                         <div className='row -intro-y'>
-                            <div className='col-3'>Specification:</div>
+                            <div className='col-3'>Specification</div>
                             <div className='col-4'>{asset.specification}</div>
                         </div>
                         <div className='row -intro-y'>
-                            <div className='col-3'>History:</div>
+                            <div className='col-3'>History</div>
                             <div>
                                 <AssetDetailTable
                                     assetHistory={null}
