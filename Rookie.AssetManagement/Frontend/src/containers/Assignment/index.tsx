6891253e165215b286e47ce836dda85341d8fa7d
Ngo Vo Tuan An <ngovotuananh@gmail.com>
2022-12-05 12:10:37 +0700

    create assignment list view FE

--- Rookie.AssetManagement/Frontend/src/containers/Assignment/index.tsx
+++ Rookie.AssetManagement/Frontend/src/containers/Assignment/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { LOGIN } from "src/constants/pages";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { ASSIGNMENT_LIST, LOGIN } from "src/constants/pages";
 
 import { useAppDispatch, useAppSelector } from "src/hooks/redux";
 import { me } from "../Authorize/reducer";
+import ListAssignment from "./List";
 
 const Assignment = () => {
     const { isAuth, account } = useAppSelector((state) => state.authReducer);
@@ -19,11 +20,11 @@ const Assignment = () => {
     }, [isAuth]);
 
     return (
-        <>
-            <div className='primaryColor text-title intro-x'>
-                Manage Assignment
-            </div>
-        </>
+        <Routes>
+            <Route path={ASSIGNMENT_LIST} element={<ListAssignment />} />
+            {/* <Route path={CREATE_ASSET} element={<CreateAsset />} />
+        <Route path={EDIT_ASSET} element={<UpdateAsset />} /> */}
+        </Routes>
     );
 };
 
