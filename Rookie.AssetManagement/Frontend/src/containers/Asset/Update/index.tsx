10280a0d6e91d153f22c8cc88b957234aec81f29
DatNgo <ngominhdat110115@gmail.com>
2022-12-06 14:00:28 +0700

    fix update asset

--- Rookie.AssetManagement/Frontend/src/containers/Asset/Update/index.tsx
+++ Rookie.AssetManagement/Frontend/src/containers/Asset/Update/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { ASSET_LIST_LINK } from "src/constants/pages";
 import { useAppDispatch, useAppSelector } from "src/hooks/redux";
 import AssetFormContainer from "../AssetForm";
 import { getAssetFormData } from "../reducer";
 
 const UpdateAssetContainer = () => {
     const { id } = useParams<{ id: string }>();
+    const navigate = useNavigate();
     const { assetFormData } = useAppSelector((state) => state.assetReducer);
     const dispatch = useAppDispatch();
     useEffect(()=>{
       dispatch(getAssetFormData(Number(id)))
     }, [])
-  
+
+    useEffect(()=>{
+      if(assetFormData?.state === 1){
+        navigate(ASSET_LIST_LINK)
+      }
+    }, [assetFormData?.state])
+      
     return (
       <div className="ml-5">
         <div className="primaryColor text-title intro-x">Edit Asset</div>
