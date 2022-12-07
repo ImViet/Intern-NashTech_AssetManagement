b8612b6a016f043a809129bb2c107e186ea559c9
DatNgo <ngominhdat110115@gmail.com>
2022-12-07 16:09:25 +0700

    fix update validate

--- Rookie.AssetManagement/Validators/UpdateAssignmentDTOValidator.cs
+++ Rookie.AssetManagement/Validators/UpdateAssignmentDTOValidator.cs
@@ -1,21 +1,23 @@
 using System;
 using FluentValidation;
 using Rookie.AssetManagement.Contracts.Dtos.AssignmentDtos;
-public class UpdateAssignmentDTOValidator : AbstractValidator<AssignmentUpdateDto> {
-    public UpdateAssignmentDTOValidator(){
-         RuleFor(assginment => assginment.User)
-            .NotEmpty()
-            .WithMessage("User is empty");
-         RuleFor(assginment => assginment.Asset)
-            .NotEmpty()
-            .WithMessage("Asset is empty");
-         RuleFor(assginment => assginment.AssignedDate)
-            .NotEmpty()
-            .WithMessage("Assgined Date is empty")
-            .GreaterThanOrEqualTo(DateTime.Now)
-            .WithMessage("Assgined Date is less than day now");
-         RuleFor(assginment => assginment.Note)
-            .NotEmpty()
-            .WithMessage("Note is empty");
+public class UpdateAssignmentDTOValidator : AbstractValidator<AssignmentUpdateDto>
+{
+    public UpdateAssignmentDTOValidator()
+    {
+        RuleFor(assginment => assginment.User)
+           .NotEmpty()
+           .WithMessage("User is empty");
+        RuleFor(assginment => assginment.Asset)
+           .NotEmpty()
+           .WithMessage("Asset is empty");
+        RuleFor(assginment => assginment.AssignedDate)
+           .NotEmpty()
+           .WithMessage("Assgined Date is empty")
+           .GreaterThanOrEqualTo(DateTime.Today)
+           .WithMessage("Assgined Date is less than day now");
+        RuleFor(assginment => assginment.Note)
+           .NotEmpty()
+           .WithMessage("Note is empty");
     }
 }
